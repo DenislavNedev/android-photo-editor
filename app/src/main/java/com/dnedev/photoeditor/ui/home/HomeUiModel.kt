3f506ad2d769b4bc36f435572828e5fcf91e86a2
DenislavNedev <dnedev98@gmail.com>
2021-01-11 01:02:59 +0200

    Added HomeUiModel. Rearranged home screen

--- app/src/main/java/com/dnedev/photoeditor/ui/home/HomeUiModel.kt
+++ app/src/main/java/com/dnedev/photoeditor/ui/home/HomeUiModel.kt
@@ -1,4 +1,34 @@
 package com.dnedev.photoeditor.ui.home
 
-class HomeUiModel {
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import com.dnedev.photoeditor.utils.EMPTY_EDIT_TEXT
+import kotlin.properties.Delegates
+import androidx.databinding.library.baseAdapters.BR
+
+class HomeUiModel : BaseObservable() {
+    @get:Bindable
+    var searchQuery: String by Delegates.observable(EMPTY_EDIT_TEXT) { _, _, _ ->
+        notifyPropertyChanged(BR.searchQuery)
+    }
+
+    @get:Bindable
+    var searchQueryError: String? by Delegates.observable(null) { _, _, _ ->
+        notifyPropertyChanged(BR.searchQueryError)
+    }
+
+    @get:Bindable
+    var areTherePhotos: Boolean by Delegates.observable(false) { _, _, _ ->
+        notifyPropertyChanged(BR.areTherePhotos)
+    }
+
+    @get:Bindable
+    var isLoadingInitially: Boolean by Delegates.observable(false) { _, _, _ ->
+        notifyPropertyChanged(BR.loadingInitially)
+    }
+
+    @get:Bindable
+    var areLoadingMorePhotos: Boolean by Delegates.observable(false) { _, _, _ ->
+        notifyPropertyChanged(BR.areLoadingMorePhotos)
+    }
 }
--- app/src/main/java/com/dnedev/photoeditor/utils/Constants.kt
+++ app/src/main/java/com/dnedev/photoeditor/utils/Constants.kt
@@ -4,3 +4,4 @@ const val AUTHORIZATION_HEADER = "Authorization"
 const val CACHE_DIRECTORY = ""
 const val DEFAULT_PAGE = 1
 const val DEFAULT_ITEMS_PER_PAGE = 20
+const val EMPTY_EDIT_TEXT = ""
