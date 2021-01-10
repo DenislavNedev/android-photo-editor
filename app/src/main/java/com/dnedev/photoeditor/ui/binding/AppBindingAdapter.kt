9afccc266ba662874c0504af9f9501a28a9f15bd
DenislavNedev <dnedev98@gmail.com>
2021-01-11 01:51:24 +0200

    Added photo loading from url

--- app/src/main/java/com/dnedev/photoeditor/ui/binding/AppBindingAdapter.kt
+++ app/src/main/java/com/dnedev/photoeditor/ui/binding/AppBindingAdapter.kt
@@ -1,7 +1,12 @@
 package com.dnedev.photoeditor.ui.binding
 
+import android.graphics.BitmapFactory
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.dnedev.photoeditor.R
+import kotlinx.coroutines.*
+import java.net.URL
 
 object AppBindingAdapter {
 
@@ -11,5 +16,25 @@ object AppBindingAdapter {
         isVisible: Boolean
     ) {
         view.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+    @BindingAdapter("loadPhotoFromUrl")
+    fun loadPhotoFromUrl(
+        imageView: ImageView,
+        url: String
+    ) {
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                getBitmapFromUrl(url).let {
+                    imageView.setImageBitmap(it)
+                }
+            } catch (exception: Exception) {
+                imageView.setImageResource(R.drawable.ic_photo_not_found)
+            }
+        }
+    }
+
+    private suspend fun getBitmapFromUrl(url: String) = withContext(Dispatchers.IO) {
+        BitmapFactory.decodeStream(URL(url).openStream())
     }
 }
